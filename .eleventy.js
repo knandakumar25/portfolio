740bcfff9a2c9f8374327db021be7d8245c853ac
Karthik Nandakumar <karthiknk21@gmail.com>
2025-06-21 17:16:25 -0400

    Fixed paths

--- .eleventy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (eleventyConfig) {
-    return {
-        pathPrefix: "/portfolio/",
-        templateFormats: ["html", "njk"],
-        htmlTemplateEngine: "njk"
-    };
-};
